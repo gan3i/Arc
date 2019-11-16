32af5241462749bc779f343829cb33f312534b2d
Hiren Dhinoja <hiren.dhinoja@powerschool.com>
2019-11-16 22:43:10 +0530

    changed version to work with vs2019

--- Arc.DataVisualizers/Arc.DataVisualizers/ListVisualizer/ListVisualizerForm.Designer.cs
+++ Arc.DataVisualizers/Arc.DataVisualizers/ListVisualizer/ListVisualizerForm.Designer.cs
@@ -39,18 +39,20 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.gridData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridData.Location = new System.Drawing.Point(12, 12);
+            this.gridData.Location = new System.Drawing.Point(16, 13);
+            this.gridData.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gridData.Name = "gridData";
-            this.gridData.Size = new System.Drawing.Size(397, 216);
+            this.gridData.Size = new System.Drawing.Size(782, 492);
             this.gridData.TabIndex = 0;
             // 
             // btnClose
             // 
             this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnClose.Location = new System.Drawing.Point(334, 234);
+            this.btnClose.Location = new System.Drawing.Point(698, 512);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.Size = new System.Drawing.Size(100, 28);
             this.btnClose.TabIndex = 1;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
@@ -58,12 +60,13 @@
             // 
             // ListVisualizerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnClose;
-            this.ClientSize = new System.Drawing.Size(421, 269);
+            this.ClientSize = new System.Drawing.Size(814, 555);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.gridData);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ListVisualizerForm";
             this.Text = "List Visualizer";
             this.Load += new System.EventHandler(this.ListVisualizerForm_Load);
